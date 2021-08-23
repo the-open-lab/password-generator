ec0f5cdef3ab3737397d626681d18e6ba0ec4af8
Jonathan Landrum <me@jonlandrum.com>
2021-08-23 12:15:11 -0500

    Clean up merge artifacts

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "mainwindow.h"
 #include "dialog.h"
 #include "ui_mainwindow.h"
@@ -46,38 +45,3 @@ void MainWindow::on_actionAbout_triggered() {
   Dialog *dialog = new Dialog();
   dialog->exec();
 }
-||||||| empty tree
-=======
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow) {
-  ui->setupUi(this);
-}
-
-MainWindow::~MainWindow() { delete ui; }
-
-void MainWindow::on_allowRepeatsCheckBox_stateChanged() {
-  toggleMaximumRepeats();
-}
-
-void MainWindow::on_maximumRepeatsCheckBox_stateChanged() {
-  toggleMaximumRepeats();
-}
-
-void MainWindow::toggleMaximumRepeats() {
-  ui->maximumRepeatsSpinBox->setEnabled(
-      ui->allowRepeatsCheckBox->isChecked() &&
-      ui->maximumRepeatsCheckBox->isChecked());
-}
-
-void MainWindow::on_copyPasswordButton_clicked() {
-  ui->passwordLineEdit->selectAll();
-  ui->passwordLineEdit->copy();
-}
-
-void MainWindow::on_generatePasswordButton_clicked() {
-  ui->passwordLineEdit->setText("Success");
-}
->>>>>>> f2d7a06d0ec3f30d6445bb38fd65ac390d243b62
--- mainwindow.h
+++ mainwindow.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -34,39 +33,3 @@ private:
   void toggleMaximumRepeats();
 };
 #endif // MAINWINDOW_H
-||||||| empty tree
-=======
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow {
-  Q_OBJECT
-
-public:
-  MainWindow(QWidget *parent = nullptr);
-  ~MainWindow();
-
-private slots:
-  void on_allowRepeatsCheckBox_stateChanged();
-
-  void on_maximumRepeatsCheckBox_stateChanged();
-
-  void on_copyPasswordButton_clicked();
-
-  void on_generatePasswordButton_clicked();
-
-private:
-  Ui::MainWindow *ui;
-
-  void toggleMaximumRepeats();
-};
-#endif // MAINWINDOW_H
->>>>>>> f2d7a06d0ec3f30d6445bb38fd65ac390d243b62
